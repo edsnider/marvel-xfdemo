fc83628ce0b1a971fa9e5ad82ef95b1845afafc9
Ed Snider <edsnider@live.com>
2016-07-05 14:57:21 -0400

    added datapages sample

---
+++ UsingDataPages/MarvelDemo/MarvelDemo/MarvelDemo/Views/CharacterView.xaml.cs
@@ -0,0 +1,12 @@
+﻿using Xamarin.Forms.Pages;
+
+namespace MarvelDemo.Views
+{
+    public partial class CharacterView : DataPage
+    {
+        public CharacterView()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ UsingDataPages/MarvelDemo/MarvelDemo/MarvelDemo/Views/MainView.xaml.cs
@@ -0,0 +1,12 @@
+﻿using Xamarin.Forms.Pages;
+
+namespace MarvelDemo.Views
+{
+    public partial class MainView : ListDataPage
+    {
+        public MainView()
+        {
+            InitializeComponent();     
+        }
+    }
+}
