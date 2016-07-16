5aa840a0e1c5562e0690522f895a5fd802bb4f95
Ed Snider <edsnider@live.com>
2016-07-16 12:52:48 -0400

    added material design resources for Android project

--- MarvelDemo/MarvelDemo/MarvelDemo.Droid/MainActivity.cs
+++ MarvelDemo/MarvelDemo/MarvelDemo.Droid/MainActivity.cs
@@ -1,14 +1,18 @@
 ﻿using Android.App;
 using Android.Content.PM;
 using Android.OS;
+using Xamarin.Forms.Platform.Android;
 
 namespace MarvelDemo.Droid
 {
     [Activity(Label = "MarvelDemo", Icon = "@drawable/icon", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
-    public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsApplicationActivity
+    public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
     {
         protected override void OnCreate(Bundle bundle)
         {
+            FormsAppCompatActivity.ToolbarResource = Resource.Layout.toolbar;
+            FormsAppCompatActivity.TabLayoutResource = Resource.Layout.tabs;
+
             base.OnCreate(bundle);
 
             global::Xamarin.Forms.Forms.Init(this, bundle);
