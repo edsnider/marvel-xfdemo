236ef8be1c34bd67bd6c07f960e5ced36001ca4f
Ed Snider <edsnider@live.com>
2016-07-19 15:17:07 -0400

    added About view

---
+++ MarvelDemo/MarvelDemo/MarvelDemo/Views/AboutView.xaml.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Xamarin.Forms;
+
+namespace MarvelDemo.Views
+{
+    public partial class AboutView
+    {
+        public AboutView()
+        {
+            InitializeComponent();
+        }
+
+        private void MarvelLinkButton_OnClicked(object sender, EventArgs e)
+        {
+            Device.OpenUri(new Uri("http://developer.marvel.com"));
+        }
+
+        private void Icons8LinkButton_OnClicked(object sender, EventArgs e)
+        {
+            Device.OpenUri(new Uri("http://www.icons8.com"));
+        }
+    }
+}
